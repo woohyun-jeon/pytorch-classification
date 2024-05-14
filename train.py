10e7c9adeaed44c3521a23dc559fa8e476a16bce
woohyuun <jwh2204@gmail.com>
2024-05-14 10:33:05 +0900

    Minor Bug Fix

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ import os
 import random
 import argparse
 import numpy as np
-from tqdm import tqdm
+from tqdm.auto import tqdm
 import matplotlib.pyplot as plt
 
 import models
@@ -11,15 +11,16 @@ import torch
 import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
+from torchvision.datasets import ImageFolder
 
 
 def get_arguments():
     parser = argparse.ArgumentParser(description='parameters for image classification model using ImageNet dataset')
-    parser.add_argument('--model_name', type=str, default='vgg11', help='model name', required=True)
-    parser.add_argument('--data_dir', type=str, help='dataset path', required=True)
-    parser.add_argument('--output_dir', type=str, help='output path', required=True)
-    parser.add_argument('--epochs', type=int, default=1000, help='number of epochs')
-    parser.add_argument('--batch_size', type=int, default=32, help='batch size')
+    parser.add_argument('--model_name', type=str, default='vgg11', help='model name')
+    parser.add_argument('--data_dir', type=str, default='E:/test/classification/data', help='dataset path')
+    parser.add_argument('--output_dir', type=str, default='E:/test/classification/outputs', help='output path')
+    parser.add_argument('--epochs', type=int, default=500, help='number of epochs')
+    parser.add_argument('--batch_size', type=int, default=16, help='batch size')
     parser.add_argument('--num_workers', type=int, default=4, help='number of workers')
     parser.add_argument('--seed', type=int, default=123, help='random seed')
     parser.add_argument('--learning_rate', type=float, default=0.0001, help='learning rate')
@@ -33,7 +34,7 @@ def train(model, device, train_dataloader, optimizer, criterion):
     model.train()
 
     train_loss = 0.
-    for batch_idx, (data, target) in tqdm(enumerate(train_dataloader)):
+    for batch_idx, (data, target) in tqdm(enumerate(train_dataloader), desc='-- Training --', total=len(train_dataloader)):
         data, target = data.to(device), target.to(device)
 
         # feed forward
@@ -60,7 +61,7 @@ def valid(model, device, valid_dataloader, criterion):
     total = 0
     correct = 0
     with torch.no_grad():
-        for batch_idx, (data, target) in tqdm(enumerate(valid_dataloader)):
+        for batch_idx, (data, target) in tqdm(enumerate(valid_dataloader), desc='-- Validation --', total=len(valid_dataloader)):
             data, target = data.to(device), target.to(device)
 
             # get predicted
@@ -96,25 +97,21 @@ def main():
     transform_train = transforms.Compose([
         transforms.Resize(size=(224,224)),
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.4914,0.4822,0.4465], std=[0.2023,0.1994,0.2010])
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
     transform_valid = transforms.Compose([
         transforms.Resize(size=(224,224)),
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.4914,0.4822,0.4465], std=[0.2023,0.1994,0.2010])
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
 
-    # train_dataset = torchvision.datasets.ImageNet(root=args.data_dir, split='train', transform=transform_train)
-    # valid_dataset = torchvision.datasets.ImageNet(root=args.data_dir, split='val', transform=transform_valid)
-    train_dataset = torchvision.datasets.STL10(root=args.data_dir, download=True, split='train', transform=transform_train)
-    valid_dataset = torchvision.datasets.STL10(root=args.data_dir, download=True, split='test', transform=transform_valid)
-    train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size,
-                                                   shuffle=True, num_workers=args.num_workers)
-    valid_dataloader = torch.utils.data.DataLoader(valid_dataset, batch_size=args.batch_size,
-                                                   shuffle=False, num_workers=args.num_workers)
+    train_dataset = ImageFolder(root=os.path.join(args.data_dir, 'train'), transform=transform_train)
+    valid_dataset = ImageFolder(root=os.path.join(args.data_dir, 'val'), transform=transform_valid)
+    train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers)
+    valid_dataloader = torch.utils.data.DataLoader(valid_dataset, batch_size=args.batch_size, shuffle=False, num_workers=args.num_workers)
 
     # set model
-    model = models.__dict__[args.model_name](in_channels=3, num_classes=10).to(device)
+    model = models.__dict__[args.model_name](in_channels=3, num_classes=len(train_dataset.classes)).to(device)
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=args.learning_rate, momentum=0.9, weight_decay=5e-4)
     scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer=optimizer, T_max=200)
@@ -125,6 +122,7 @@ def main():
     best_acc = 0.
     acc_patience = 0
     for epoch in range(args.epochs):
+        print('[Start] Epoch {}/{}'.format(epoch + 1, args.epochs))
         train_loss = train(model, device, train_dataloader, optimizer, criterion)
         valid_loss, valid_acc = valid(model, device, valid_dataloader, criterion)
         train_losses.append(train_loss)
@@ -136,6 +134,8 @@ def main():
               'val acc: {:.4f}'.format(valid_acc*100)
               )
 
+        print('[Complete] Epoch {}/{}'.format(epoch + 1, args.epochs))
+
         scheduler.step()
 
         if abs(valid_acc - best_acc) < 0.0001:
@@ -146,7 +146,7 @@ def main():
 
         if valid_acc > best_acc:
             best_acc = valid_acc
-            torch.save(model.state_dict(), os.path.join(args.output_dir, args.model_name + '_best.pth'))
+            torch.save(model.state_dict(), os.path.join(args.output_dir, args.model_name + '_imagenet_224.pth'))
 
 
     # save loss changes to image file
@@ -156,9 +156,9 @@ def main():
     plt.title('Train-Valid Loss', fontsize=20)
     plt.xlabel('Epochs', fontsize=15)
     plt.ylabel('Loss', fontsize=15)
-    plt.xticks(fontsize=12.5)
-    plt.yticks(fontsize=12.5)
-    plt.legend(fontsize=17.5)
+    plt.xticks(fontsize=10)
+    plt.yticks(fontsize=10)
+    plt.legend(fontsize=10)
     plt.savefig(os.path.join(args.output_dir, args.model_name + '_loss.png'))
 
 
